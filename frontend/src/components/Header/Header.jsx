24d1ae8a3150c796f00825f3c2394ee11969f921
AnvithFoxi <anvithraju007@gmail.com>
2024-06-21 12:05:03 +0530

    first commit

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className='header-contents'>
+            <h2>Order your favourite food here</h2>
+            <p>ndulge in flavors that dance on your palate, crafted with passion and precision at our restaurant. Each dish tells a story of tradition and innovation, promising an unforgettable culinary journey with every bite</p>
+            <button>View Menu</button>
+
+        </div>
+
+    </div>
+  )
+}
+
+export default Header
